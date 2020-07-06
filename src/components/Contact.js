bb92c271f5e1879ddd1bf898ec6948aeef824d1f
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-07-06 11:50:26 +0200

    first manual commit

---
+++ src/components/Contact.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './components/contact.css';
+
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?inc=name,picture")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Remarque : il est important de traiter les erreurs ici
+        // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+        // des exceptions provenant de réels bugs du composant.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Chargement…</div>;
+    } else {
+      return (
+        <div className="Contact" >
+          <img className="avatar" src={items.picture.thumbnail}/>
+          <div>
+            <div className="name">{items.name.map(item => (`${item.title},${item.first},${item.last}`))}</div>
+            <div className="status">
+              <div className="status-online"></div>
+              <div className="status-text">Online</div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
